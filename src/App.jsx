9b9cd46efaefc904222467bb0622ca96fa104833
vikas-git1 <vikasmewara.dev@gmail.com>
2024-08-30 08:16:13 +0530

    cleanup react default code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ConvertTemp from "./components/ConvertTemp";
+
+function App() {
+  return (
+    <>
+      <h1>Ram Ram ji</h1>
+      <ConvertTemp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ConvertTemp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConvertTemp = () => {
+  return <div>Hwllow</div>;
+};
+
+export default ConvertTemp;
